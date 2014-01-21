bb67b983786e41675d2001ba322b279102f25608
John Grubb <therealjohnnygrubb@gmail.com>
2014-01-21 13:24:43 +0000

    removing sqlite

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ IbdCom::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
@@ -71,6 +71,7 @@ IbdCom::Application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = { :host => 'ignoredbydinosaurs.com' }
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
