0bf84555f19961484b927767cd2655544c135cd5
John Grubb <therealjohnnygrubb@gmail.com>
2014-01-23 08:33:38 -0500

    trimming down path_date_path_helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,11 +2,7 @@ module PostsHelper
   require 'date'
 
   def post_date_path_helper post
-    year = post.created_at.year
-    month = post.created_at.month.to_s
-    month = "0#{month}" if month.size == 1
-    slug = post.slug
-    post_date_path(year, month, slug)
+    post_date_path(post.year, post.month, post.slug)
   end
 
 end
