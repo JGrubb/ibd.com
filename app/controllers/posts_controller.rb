3abf9cde0ffdcbb01e391fd71b578d570ff4c2dd
John Grubb <therealjohnnygrubb@gmail.com>
2014-01-29 15:13:29 -0500

    adding delete link to post edit form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -89,7 +89,7 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url }
+      format.html { redirect_to post_index_url }
       format.json { head :no_content }
     end
   end
