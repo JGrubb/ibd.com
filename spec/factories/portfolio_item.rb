9a96fd007707829c97717e03e5d480461e3a0229
John Grubb <therealjohnnygrubb@gmail.com>
2014-02-03 08:05:33 -0500

    nerging

---
+++ spec/factories/portfolio_item.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :portfolio_item do
+    title "The Title"
+    featured_image "test.jpg"
+  end
+end
---
+++ spec/factories/post.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :post do
+    title "The title"
+  end
+end
