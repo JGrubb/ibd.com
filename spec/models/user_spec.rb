0c9d3188c6210b70c0ddaf9e0f81f57442e5fe75
John Grubb <therealjohnnygrubb@gmail.com>
2014-02-08 10:29:35 -0500

    adding feature specs, begin

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,37 @@
 require 'spec_helper'
 
 describe User do
-  
+  it "is invalid without an email address" do
+    user = FactoryGirl.attributes_for(:user)
+    user[:email] = nil
+    expect(user).to have(3).errors_on(:email)
+  end
+
+  it "is invalid without a unique email" do
+    user1 = create(:user, email: "test@example.com")
+    user2 = User.new(
+      password: "12345678", 
+      password_confirmation: "12345678", 
+      email: "test@example.com"
+    )
+    expect(user2).to have(1).error_on :email
+  end
+
+  it "is invalid with an incorrect password confirmation" do
+    user = User.new(
+      password: "12345678", 
+      password_confirmation: "13264578", 
+      email: "test@example.com"
+    )
+    expect(user).to have(1).error_on :password_confirmation
+  end
+
+  it "is valid with a email, pass, and confirmation" do
+    user = User.new(
+      password: "12345678",
+      password_confirmation: "12345678",
+      email: "test@example.com"
+    )
+    expect(user).to be_valid
+  end
 end
--- spec/requests/portfolio_items_spec.rb
+++ spec/requests/portfolio_items_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe "PortfolioItems" do
   describe "GET /portfolio" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+    it "gets the portfolio index" do
       get portfolio_items_path
       response.status.should be(200)
     end
   end
+
 end
