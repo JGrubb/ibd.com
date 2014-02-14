40f951a917ab54dae588e0d6b0d7ba9b1fee01c3
John Grubb <therealjohnnygrubb@gmail.com>
2014-02-14 08:33:09 -0500

    downcasing all tags before save

--- app/models/post.rb
+++ app/models/post.rb
@@ -26,11 +26,11 @@ class Post < ActiveRecord::Base
   end
 
   def tag_list
-    tags.map(&:name).join(", ")
+    tags.map(&:name).join(", ").titleize
   end
 
   def tag_list=(names)
-    self.tags = names.split(",").map do |n|
+    self.tags = names.downcase.split(",").map do |n|
       Tag.where(name: n.strip).first_or_create!
     end
   end
