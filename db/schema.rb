f3f4b4a7167abe2546b824f7dbf977f47f2618f7
John Grubb <therealjohnnygrubb@gmail.com>
2014-02-25 16:11:07 -0500

    switching back to Ruby schema def

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140110141927) do
+ActiveRecord::Schema.define(version: 20140210200727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_trgm"
 
   create_table "friendly_id_slugs", force: true do |t|
     t.string   "slug",                      null: false
@@ -29,6 +30,18 @@ ActiveRecord::Schema.define(version: 20140110141927) do
   add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
 
+  create_table "images", force: true do |t|
+    t.string   "caption"
+    t.string   "image"
+    t.integer  "imageable_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "imageable_type", default: "PortfolioItem"
+  end
+
+  add_index "images", ["imageable_id"], name: "index_images_on_imageable_id", using: :btree
+  add_index "images", ["imageable_type"], name: "index_images_on_imageable_type", using: :btree
+
   create_table "portfolio_items", force: true do |t|
     t.string   "title"
     t.text     "body"
@@ -36,6 +49,8 @@ ActiveRecord::Schema.define(version: 20140110141927) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "slug"
+    t.text     "summary"
+    t.text     "sidebar"
   end
 
   add_index "portfolio_items", ["slug"], name: "index_portfolio_items_on_slug", using: :btree
@@ -52,5 +67,45 @@ ActiveRecord::Schema.define(version: 20140110141927) do
 
   add_index "posts", ["published"], name: "index_posts_on_published", using: :btree
   add_index "posts", ["slug"], name: "index_posts_on_slug", using: :btree
+
+  create_table "taggings", force: true do |t|
+    t.integer  "post_id"
+    t.integer  "tag_id"
+    t.text     "info"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "taggings", ["post_id"], name: "index_taggings_on_post_id", using: :btree
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
