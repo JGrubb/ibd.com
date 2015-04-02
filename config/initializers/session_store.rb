44fb32c766e56c0f3e46c7c65e4ff66b0c9abaf4
John Grubb <john.grubb@advantagemedia.com>
2015-04-02 15:24:56 -0400

    moving session to redis to try and fix caching/header problems, also Skeleton CSS

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-IbdCom::Application.config.session_store :cookie_store, key: '_ibd_com_session'
+IbdCom::Application.config.session_store :redis_store, key: '_ibd_com_session'
