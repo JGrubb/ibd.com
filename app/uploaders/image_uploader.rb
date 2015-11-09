5e8007b16999934d0f5ee7026b1311702b53c649
John Grubb <therealjohnnygrubb@gmail.com>
2015-11-09 11:28:38 -0500

    small tweeks

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -43,7 +43,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :full do
-    process resize_to_fill: [800, 800]
+    process resize_to_fit: [800, 800]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
